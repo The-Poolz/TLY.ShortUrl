3aef3ead0ce572c38d887525acb5f6e158b1aee9
Stan Goldin <salty87@gmail.com>
2024-05-05 16:47:59 +0300

    Add project files.

---
+++ ShortenedLinkResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace TLY.ShortUrl;
+
+public class ShortenedLinkResponse
+{
+    public string short_url { get; set; }
+    public string description { get; set; }
+    public string long_url { get; set; }
+    public string domain { get; set; }
+    public string short_id { get; set; }
+    public int? expire_at_views { get; set; }
+    public DateTime? expire_at_datetime { get; set; }
+    public bool public_stats { get; set; }
+    public DateTime created_at { get; set; }
+    public DateTime updated_at { get; set; }
+}
---
+++ TlyContext.cs
@@ -0,0 +1,25 @@
+﻿using Flurl.Http;
+
+namespace TLY.ShortUrl;
+
+public class TlyContext(string apiKey)
+{
+    public async Task<ShortenedLinkResponse> GetShortUrlAsync(string LongUrl, string Description, string Domain = "https://t.ly", bool PublicStats = true) =>
+        await GetResponse(await GetFlurlResponse(LongUrl, Description, Domain, PublicStats));
+
+    public virtual Task<ShortenedLinkResponse> GetResponse(IFlurlResponse? response) =>
+        response == null ? throw new Exception() : response.GetJsonAsync<ShortenedLinkResponse>();
+
+    public virtual Task<IFlurlResponse?> GetFlurlResponse(string LongUrl, string Description, string Domain, bool PublicStats) =>
+        "https://t.ly/api/v1/link/shorten"
+            .WithHeader("Authorization", $"Bearer {apiKey}")
+            .WithHeader("Content-Type", "application/json")
+            .WithHeader("Accept", "application/json")
+            .PostJsonAsync(new
+            {
+                long_url = LongUrl,
+                domain = Domain,
+                description = Description,
+                public_stats = PublicStats
+            });
+}
