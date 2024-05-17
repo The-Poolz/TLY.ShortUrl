2fbf83325c35e9e8b37b78dc7375d2bf7c7f5307
Artem Krol <artemkrol47@gmail.com>
2024-05-17 04:41:07 -0500

    Use netstandard2.1

--- src/TlyContext.cs
+++ src/TlyContext.cs
@@ -1,29 +1,30 @@
 ﻿using Flurl.Http;
 
-namespace TLY.ShortUrl;
-
-public class TlyContext
+namespace TLY.ShortUrl
 {
-    private readonly string _apiKey;
+    public class TlyContext
+    {
+        private readonly string _apiKey;
 
-    public TlyContext(string apiKey) => this._apiKey = apiKey;
+        public TlyContext(string apiKey) => this._apiKey = apiKey;
 
-    public async Task<ShortenedLinkResponse> GetShortUrlAsync(string longUrl, string description, string domain = "https://t.ly", bool publicStats = true) =>
-        await GetResponse(await GetFlurlResponse(longUrl, description, domain, publicStats));
+        public async Task<ShortenedLinkResponse> GetShortUrlAsync(string longUrl, string description, string domain = "https://t.ly", bool publicStats = true) =>
+            await GetResponse(await GetFlurlResponse(longUrl, description, domain, publicStats));
 
-    public virtual Task<ShortenedLinkResponse> GetResponse(IFlurlResponse? response) =>
-        response == null ? throw new Exception() : response.GetJsonAsync<ShortenedLinkResponse>();
+        public virtual Task<ShortenedLinkResponse> GetResponse(IFlurlResponse? response) =>
+            response == null ? throw new Exception() : response.GetJsonAsync<ShortenedLinkResponse>();
 
-    public virtual Task<IFlurlResponse?> GetFlurlResponse(string longUrl, string description, string domain, bool publicStats) =>
-        "https://t.ly/api/v1/link/shorten"
-            .WithHeader("Authorization", $"Bearer {_apiKey}")
-            .WithHeader("Content-Type", "application/json")
-            .WithHeader("Accept", "application/json")
-            .PostJsonAsync(new
-            {
-                long_url = longUrl,
-                domain,
-                description,
-                public_stats = publicStats
-            });
+        public virtual Task<IFlurlResponse?> GetFlurlResponse(string longUrl, string description, string domain, bool publicStats) =>
+            "https://t.ly/api/v1/link/shorten"
+                .WithHeader("Authorization", $"Bearer {_apiKey}")
+                .WithHeader("Content-Type", "application/json")
+                .WithHeader("Accept", "application/json")
+                .PostJsonAsync(new
+                {
+                    long_url = longUrl,
+                    domain,
+                    description,
+                    public_stats = publicStats
+                });
+    }
 }
