1fbc6559013a5952619dd2f4b328a50f9c80c95b
Stanislav Vysotskyi <ardenhide@gmail.com>
2024-06-20 15:28:08 +0300

    - Create `ITlyContext` interface - Update UI in `TlyContext`

---
+++ src/TLY.ShortUrl/ITlyContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace TLY.ShortUrl
+{
+    public interface ITlyContext
+    {
+        public Task<ShortenedLinkResponse> GetShortUrlAsync(
+            string longUrl,
+            string description,
+            string domain = "https://t.ly",
+            bool publicStats = true
+        );
+    }
+}
--- src/TLY.ShortUrl/TlyContext.cs
+++ src/TLY.ShortUrl/TlyContext.cs
@@ -4,7 +4,7 @@ using System.Threading.Tasks;
 
 namespace TLY.ShortUrl
 {
-    public class TlyContext
+    public class TlyContext : ITlyContext
     {
         private readonly string _apiKey;
 
@@ -13,10 +13,10 @@ namespace TLY.ShortUrl
         public virtual async Task<ShortenedLinkResponse> GetShortUrlAsync(string longUrl, string description, string domain = "https://t.ly", bool publicStats = true) =>
             await GetResponse(await GetFlurlResponse(longUrl, description, domain, publicStats));
 
-        public virtual Task<ShortenedLinkResponse> GetResponse(IFlurlResponse? response) =>
+        private Task<ShortenedLinkResponse> GetResponse(IFlurlResponse? response) =>
             response == null ? throw new InvalidOperationException("Failed to receive a valid response from the external service.") : response.GetJsonAsync<ShortenedLinkResponse>();
 
-        public virtual Task<IFlurlResponse?> GetFlurlResponse(string longUrl, string description, string domain, bool publicStats) =>
+        private Task<IFlurlResponse?> GetFlurlResponse(string longUrl, string description, string domain, bool publicStats) =>
             "https://t.ly/api/v1/link/shorten"
                 .WithHeader("Authorization", $"Bearer {_apiKey}")
                 .WithHeader("Content-Type", "application/json")
