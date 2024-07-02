ecec5cf81bc8f675e456823389fecced38562eb5
ArdenHide <ardenhide@gmail.com>
2024-07-02 09:31:30 +0300

    - mark `private` methods as `internal` for easy testing in `TlyContext` - add `AssemblyInfo` - added mock http responses in raw json - added tests for `ParseResponseAsync`

---
+++ tests/TLY.ShortUrl.Tests/MockResponses/MockSearchShortUrl.cs
@@ -0,0 +1,104 @@
+﻿namespace TLY.ShortUrl.Tests.MockResponses;
+
+internal static class MockSearchShortUrl
+{
+    internal static string Response => @"
+    {
+        ""current_page"": 1,
+        ""data"": [
+            {
+                ""description"": ""TEST DESCRIPTION #1"",
+                ""long_url"": ""https://www.google.com"",
+                ""short_url"": ""https://t.ly/vVRMv"",
+                ""domain"": ""https://t.ly/"",
+                ""short_id"": ""vVRMv"",
+                ""user_id"": 123,
+                ""team_id"": 123,
+                ""domain_id"": null,
+                ""safe_link"": null,
+                ""expire_at_datetime"": null,
+                ""expire_at_views"": null,
+                ""public_stats"": true,
+                ""meta"": {
+                    ""smart_urls"": []
+                },
+                ""created_at"": ""2024-07-01T16:20:27.000000Z"",
+                ""updated_at"": ""2024-07-01T16:20:27.000000Z"",
+                ""has_password"": false,
+                ""short_stats"": null,
+                ""user"": {
+                    ""id"": 123,
+                    ""name"": ""UserName"",
+                    ""email"": ""username@google.com"",
+                    ""photo_url"": ""https://www.gravatar.com/avatar/123.jpg?s=200&d=mm"",
+                    ""current_team_id"": 123,
+                    ""created_at"": ""2024-04-28T08:41:46.000000Z"",
+                    ""updated_at"": ""2024-07-01T09:20:02.000000Z""
+                },
+                ""qr_code"": null,
+                ""tags"": [],
+                ""pixels"": []
+            },
+            {
+                ""description"": ""TEST DESCRIPTION #1"",
+                ""long_url"": ""https://www.google.com"",
+                ""short_url"": ""https://t.ly/XGxcx"",
+                ""domain"": ""https://t.ly/"",
+                ""short_id"": ""XGxcx"",
+                ""user_id"": 123,
+                ""team_id"": 123,
+                ""domain_id"": null,
+                ""safe_link"": null,
+                ""expire_at_datetime"": null,
+                ""expire_at_views"": null,
+                ""public_stats"": true,
+                ""meta"": {
+                    ""smart_urls"": []
+                },
+                ""created_at"": ""2024-07-01T13:40:09.000000Z"",
+                ""updated_at"": ""2024-07-01T13:40:09.000000Z"",
+                ""has_password"": false,
+                ""short_stats"": null,
+                ""user"": {
+                    ""id"": 123,
+                    ""name"": ""UserName"",
+                    ""email"": ""username@google.com"",
+                    ""photo_url"": ""https://www.gravatar.com/avatar/123.jpg?s=200&d=mm"",
+                    ""current_team_id"": 123,
+                    ""created_at"": ""2024-04-28T08:41:46.000000Z"",
+                    ""updated_at"": ""2024-07-01T09:20:02.000000Z""
+                },
+                ""qr_code"": null,
+                ""tags"": [],
+                ""pixels"": []
+            }
+        ],
+        ""first_page_url"": ""https://api.t.ly/api/v1/link/list?page=1"",
+        ""from"": 1,
+        ""last_page"": 1,
+        ""last_page_url"": ""https://api.t.ly/api/v1/link/list?page=1"",
+        ""links"": [
+            {
+                ""url"": null,
+                ""label"": ""&laquo; Previous"",
+                ""active"": false
+            },
+            {
+                ""url"": ""https://api.t.ly/api/v1/link/list?page=1"",
+                ""label"": ""1"",
+                ""active"": true
+            },
+            {
+                ""url"": null,
+                ""label"": ""Next &raquo;"",
+                ""active"": false
+            }
+        ],
+        ""next_page_url"": null,
+        ""path"": ""https://api.t.ly/api/v1/link/list"",
+        ""per_page"": 15,
+        ""prev_page_url"": null,
+        ""to"": 2,
+        ""total"": 2
+    }";
+}
---
+++ tests/TLY.ShortUrl.Tests/TlyContextTests.cs
@@ -0,0 +1,115 @@
+﻿using Flurl.Http;
+using System.Net;
+using FluentAssertions;
+using System.Globalization;
+using TLY.ShortUrl.Responses;
+using TLY.ShortUrl.Tests.MockResponses;
+
+namespace TLY.ShortUrl.Tests;
+
+public class TlyContextTests
+{
+    public class ParseResponseAsync
+    {
+        [Fact]
+        internal async Task WhenFlurlResponseNotNull_ShouldParseResponseToModel_ForCreateShortUrlResponse()
+        {
+            var mockResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(MockCreateShortUrl.Response)
+            };
+            var flurlResponse = new FlurlResponse(new FlurlCall
+            {
+                HttpResponseMessage = mockResponse,
+                Request = new FlurlRequest()
+            });
+
+            var result = await TlyContext.ParseResponseAsync<CreateShortUrlResponse>(flurlResponse);
+
+            result.Should().NotBeNull();
+            result.Description.Should().Be("TEST DESCRIPTION #1");
+            result.LongUrl.Should().Be("https://www.google.com");
+            result.ShortUrl.Should().Be("https://t.ly/hNie_");
+            result.Domain.Should().Be("https://t.ly/");
+            result.ShortId.Should().Be("hNie_");
+            result.ExpireAtDatetime.Should().BeNull();
+            result.ExpireAtViews.Should().BeNull();
+            result.PublicStats.Should().BeTrue();
+            result.CreatedAt.Should().Be(DateTime.Parse("2024-07-02T05:18:25.000000Z", new DateTimeFormatInfo()).ToUniversalTime());
+            result.UpdatedAt.Should().Be(DateTime.Parse("2024-07-02T05:18:25.000000Z", new DateTimeFormatInfo()).ToUniversalTime());
+        }
+
+        [Fact]
+        internal async Task WhenFlurlResponseNotNull_ShouldParseResponseToModel_ForSearchShortUrlResponse()
+        {
+            var mockResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent(MockSearchShortUrl.Response)
+            };
+            var flurlResponse = new FlurlResponse(new FlurlCall
+            {
+                HttpResponseMessage = mockResponse,
+                Request = new FlurlRequest()
+            });
+
+            var result = await TlyContext.ParseResponseAsync<SearchShortUrlResponse>(flurlResponse);
+
+            result.Should().NotBeNull();
+            result.CurrentPage.Should().Be(1);
+            result.Data.Should().HaveCount(2);
+
+            var firstItem = result.Data.First();
+            firstItem.Description.Should().Be("TEST DESCRIPTION #1");
+            firstItem.LongUrl.Should().Be("https://www.google.com");
+            firstItem.ShortUrl.Should().Be("https://t.ly/vVRMv");
+            firstItem.Domain.Should().Be("https://t.ly/");
+            firstItem.ShortId.Should().Be("vVRMv");
+            firstItem.ExpireAtDatetime.Should().BeNull();
+            firstItem.ExpireAtViews.Should().BeNull();
+            firstItem.PublicStats.Should().BeTrue();
+            firstItem.CreatedAt.Should().Be(DateTime.Parse("2024-07-01T16:20:27.000000Z", new DateTimeFormatInfo()).ToUniversalTime());
+            firstItem.UpdatedAt.Should().Be(DateTime.Parse("2024-07-01T16:20:27.000000Z", new DateTimeFormatInfo()).ToUniversalTime());
+
+            var secondItem = result.Data.Last();
+            secondItem.Description.Should().Be("TEST DESCRIPTION #1");
+            secondItem.LongUrl.Should().Be("https://www.google.com");
+            secondItem.ShortUrl.Should().Be("https://t.ly/XGxcx");
+            secondItem.Domain.Should().Be("https://t.ly/");
+            secondItem.ShortId.Should().Be("XGxcx");
+            secondItem.ExpireAtDatetime.Should().BeNull();
+            secondItem.ExpireAtViews.Should().BeNull();
+            secondItem.PublicStats.Should().BeTrue();
+            secondItem.CreatedAt.Should().Be(DateTime.Parse("2024-07-01T13:40:09.000000Z", new DateTimeFormatInfo()).ToUniversalTime());
+            secondItem.UpdatedAt.Should().Be(DateTime.Parse("2024-07-01T13:40:09.000000Z", new DateTimeFormatInfo()).ToUniversalTime());
+        }
+
+        [Fact]
+        internal async Task ShouldThrowInvalidOperationException_WhenFlurlResponseIsNull()
+        {
+            Func<Task> testCode = async () => await TlyContext.ParseResponseAsync<CreateShortUrlResponse>(null);
+
+            await testCode.Should().ThrowAsync<InvalidOperationException>()
+                .WithMessage("Failed to receive a valid response from the external service.");
+        }
+
+        [Fact]
+        internal async Task ShouldThrowFlurlParsingException_WhenFlurlResponseIsInvalid()
+        {
+            var invalidJsonResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("Invalid JSON")
+            };
+            var flurlResponse = new FlurlResponse(new FlurlCall
+            {
+                HttpResponseMessage = invalidJsonResponse,
+                HttpRequestMessage = new HttpRequestMessage(),
+                Request = new FlurlRequest(),
+                Client = new FlurlClient()
+            });
+
+            var testCode = async () => await TlyContext.ParseResponseAsync<CreateShortUrlResponse>(flurlResponse);
+
+            await testCode.Should().ThrowAsync<FlurlParsingException>();
+        }
+    }
+}
